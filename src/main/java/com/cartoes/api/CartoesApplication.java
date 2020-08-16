099f7a4bb8ca55337a2ddc9cf6f7620f38f68721
Thiago Favero <thiaagofavero@gmail.com>
2020-08-16 19:01:47 -0300

    Arquivos iniciais

---
+++ src/main/java/com/cartoes/api/CartoesApplication.java
@@ -0,0 +1,13 @@
+package com.cartoes.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartoesApplication.class, args);
+	}
+
+}
